38dcf10b1b913cb5692353622f9203362d449faf
Davy <Davy@DESKTOP-60TC6II>
2018-04-25 03:06:09 +0800

    Add project files.

---
+++ AzureAdExample/Authentication/AzureAdOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAdExample.Authentication
+{
+    public class AzureAdOptions
+    {
+        public string ClientId { get; set; }
+
+        public string Instance { get; set; }
+
+        public string TenantId { get; set; }
+
+        public string CallbackPath { get; set; }
+
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ AzureAdExample/Cache/NaiveSessionCache.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace AzureAdExample.Cache
+{
+    public class NaiveSessionCache : TokenCache
+    {
+        private static ReaderWriterLockSlim SessionLock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+        private string UserObjectId { get; set; }
+        private string CacheId { get; set; }
+        private ISession Session { get; set; }
+        public NaiveSessionCache(string userId, ISession session)
+        {
+            UserObjectId = userId;
+            CacheId = UserObjectId + "_TokenCache";
+            Session = session;
+
+            this.AfterAccess = AfterAccessNotification;
+            this.BeforeAccess = BeforeAccessNotification;
+            Load();
+        }
+
+        public void Load()
+        {
+            SessionLock.EnterReadLock();
+            this.Deserialize(Session.Get(CacheId));
+            SessionLock.ExitReadLock();
+        }
+
+        public void Persist()
+        {
+            SessionLock.EnterWriteLock();
+
+            // Optimistically set HasStateChanged to false. We need to do it early to avoid losing changes made by a concurrent thread.
+            this.HasStateChanged = false;
+
+            // Reflect changes in the persistent store
+            Session.Set("CacheId", this.Serialize());
+            SessionLock.ExitWriteLock();
+        }
+
+        // Empties the persistent store.
+        public override void Clear()
+        {
+            base.Clear();
+            Session.Remove("CacheId");
+        }
+
+        // Triggered right before ADAL needs to access the cache.
+        // Reload the cache from the persistent store in case it changed since the last access.
+        void BeforeAccessNotification(TokenCacheNotificationArgs args)
+        {
+            Load();
+        }
+
+        // Triggered right after ADAL accessed the cache.
+        void AfterAccessNotification(TokenCacheNotificationArgs args)
+        {
+            // if the access operation resulted in a cache update
+            if (this.HasStateChanged)
+            {
+                Persist();
+            }
+        }
+    }
+}
